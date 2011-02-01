0fa7e1d73bcc23181098fbfff43489d6e16cd1e0
Fletcher T. Penney <fletcher@fletcherpenney.net>
2011-02-01 17:22:11 -0500

    Detect latex mode and use appropriate stylesheet

--- bin/mmd2xslt.pl
+++ bin/mmd2xslt.pl
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# convert to RTF using textutil - only works on Mac OS X
+# convert XHTML to LaTeX using XSLT
 
 if [ $# == 0 ]
 then
@@ -11,8 +11,20 @@ until [ "$*" = "" ]
 do
 	# process each argument separately
 	file_name=`echo $1|cut -d. -f1 `
-	/usr/local/bin/multimarkdown "$1" | xsltproc -nonet -novalid /Users/fletcher/Documents/Dropbox/Active/peg-multimarkdown/Support/XSLT/xhtml2latex.xslt -  > "$file_name.tex"
-	shift
+	
+	# Determine which stylesheet to use
+	mode=`/usr/local/bin/multimarkdown -e latexmode "$1"`
+	
+	if [ "$mode" = "memoir" ]
+	then
+		# Use memoir
+		/usr/local/bin/multimarkdown "$1" | xsltproc -nonet -novalid /Users/fletcher/Documents/Dropbox/Active/peg-multimarkdown/Support/XSLT/memoir.xslt -  > "$file_name.tex"
+		shift
+	else
+		# Use default latex
+		/usr/local/bin/multimarkdown "$1" | xsltproc -nonet -novalid /Users/fletcher/Documents/Dropbox/Active/peg-multimarkdown/Support/XSLT/xhtml2latex.xslt -  > "$file_name.tex"
+		shift
+	fi
 done
 fi
 
