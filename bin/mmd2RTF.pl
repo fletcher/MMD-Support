ba86930ff2cbdb3b1d7d6db8c9f044e2b32e64aa
Fletcher T. Penney <fletcher@fletcherpenney.net>
2011-02-06 20:14:49 -0500

    improve path handling functions to be path independent

--- bin/mmd2RTF.pl
+++ bin/mmd2RTF.pl
@@ -13,13 +13,13 @@
 if [ $# == 0 ]
 then
 	# No arguments, so use stdin/stdout
-	/usr/local/bin/multimarkdown | textutil -convert rtf -stdin -stdout 
+	multimarkdown | textutil -convert rtf -stdin -stdout 
 else
 until [ "$*" = "" ]
 do
 	# process each argument separately
 	file_name=`echo $1|cut -d. -f1 `
-	/usr/local/bin/multimarkdown "$1" | textutil -convert rtf -stdin -stdout > "$file_name.rtf"
+	multimarkdown "$1" | textutil -convert rtf -stdin -stdout > "$file_name.rtf"
 	shift
 done
 fi
